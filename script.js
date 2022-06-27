45cb5a08b5e62f552d985e798eabbea4637a5609
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-06-28 00:03:20 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// let weather = {
-//    paris: {
-//       temp: 19.7,
-//       humidity: 80,
-//    },
-//    tokyo: {
-//       temp: 17.3,
-//       humidity: 50,
-//    },
-//    lisbon: {
-//       temp: 30.2,
-//       humidity: 20,
-//    },
-//    "san francisco": {
-//       temp: 20.9,
-//       humidity: 100,
-//    },
-//    "new york": {
-//       temp: 15,
-//       humidity: 20,
-//    },
-// };
-
-// let city = prompt("Enter a city");
-// city = city.toLowerCase();
-// if (weather[city] !== undefined) {
-//    let temperature = weather[city].temp;
-//    let humidity = weather[city].humidity;
-//    let celsTemp = Math.round(temperature);
-//    let farenhTemp = Math.round((temperature * 9) / 5 + 32);
-
-//    alert(
-//       `It is currently ${celsTemp}°C (${farenhTemp}°F) in ${city} with a humidity of ${humidity}%`
-//    );
-// } else {
-//    alert(
-//       `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-//    );
-// }
-
 //date
 let today = new Date();
 let currentDate = document.querySelector("#date");
@@ -62,16 +22,6 @@ let day = days[today.getDay()];
 date.innerHTML = `${day} ${hr}:${min}`;
 
 //city
-// function search(event) {
-//    event.preventDefault();
-//    let searchInput = document.querySelector("#search-input");
-//    let city = document.querySelector("#city");
-//    city.innerHTML = searchInput.value;
-// }
-// let form = document.querySelector("#search-form");
-// form.addEventListener("submit", search);
-
-//city
 function showWeather(response) {
    document.querySelector("#city").innerHTML = response.data.name;
    document.querySelector("#degree").innerHTML = Math.round(
@@ -93,9 +43,6 @@ function searchCity(city) {
 
 function handleSearch(event) {
    event.preventDefault();
-   // let searchInput = document.querySelector("#search-input");
-   // let city = document.querySelector("#city");
-   // city.innerHTML = searchInput.value;
    let city = document.querySelector("#search-input").value;
    searchCity(city);
 }
